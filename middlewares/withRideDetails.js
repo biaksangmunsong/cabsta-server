b3f59eda4ed4980f2944d39ac963dee48c7df240
Biaksang Munsong <biaksangmunsong@gmail.com>
2022-11-08 18:02:32 +0530

    made changes to withRideDetails middleware

--- middlewares/withRideDetails.js
+++ middlewares/withRideDetails.js
@@ -8,17 +8,25 @@ module.exports = async (req, res, next) => {
         const pickupLocationLng = Number(req.query.pickupLocationLng) || NaN
         const destinationLat = Number(req.query.destinationLat) || NaN
         const destinationLng = Number(req.query.destinationLng) || NaN
-
+        
         // validate pickupLocation and destination
         if (
-            pickupLocationLat < -85 ||
-            pickupLocationLat > 85 ||
-            pickupLocationLng < -180 ||
-            pickupLocationLng > 180 ||
-            destinationLat < -85 ||
-            destinationLat > 85 ||
-            destinationLng < -180 ||
-            destinationLng > 180
+            (
+                (!pickupLocationLat && pickupLocationLat !== 0) ||
+                (!pickupLocationLng && pickupLocationLng !== 0) ||
+                (!destinationLat && destinationLat !== 0) ||
+                (!destinationLng && destinationLng !== 0)
+            ) ||
+            (
+                pickupLocationLat < -85 ||
+                pickupLocationLat > 85 ||
+                pickupLocationLng < -180 ||
+                pickupLocationLng > 180 ||
+                destinationLat < -85 ||
+                destinationLat > 85 ||
+                destinationLng < -180 ||
+                destinationLng > 180
+            )
         ){
             return next({
                 status: 406,
